2948e1a6c99221c3c00c9eccd66598c15d373413
Steve Richards <srichards@leftshiftit.com>
2017-10-09 20:48:14 +0200

    Removed - from variable names

--- vars/gitCommitPush.groovy
+++ vars/gitCommitPush.groovy
@@ -8,13 +8,13 @@ def call(Map config) {
     def branchName = sh(returnStdout: true, script: "git symbolic-ref --short HEAD").trim()
     def message = config.message
 
-    sh "git config user.email \"${config.git-email}\""
-    sh "git config --global user.name \"${config.git-user}\""
+    sh "git config user.email \"${config.email}\""
+    sh "git config --global user.name \"${config.username}\""
 
     sh "git add ${config.file}"
     sh "git commit -m \"${message}\""
 
-    withCredentials([usernamePassword(credentialsId: config.git-credentials, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: config.credentials, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
         sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${config.repo}")
     }
 }
