d308815bf09b979a95a89e5a4062866fd8765846
Steve Richards <srichards@leftshiftit.com>
2017-10-09 21:01:04 +0200

    add tag param in config

--- vars/gitCommitPush.groovy
+++ vars/gitCommitPush.groovy
@@ -14,6 +14,7 @@ def call(Map config) {
     sh "git add ${config.file}"
     sh "git commit -m \"${config.message}\""
 
+    sh "git tag -a ${config.tag} -m \"${config.message}\""
     withCredentials([usernamePassword(credentialsId: config.credentials, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
         sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${config.repo}")
     }
