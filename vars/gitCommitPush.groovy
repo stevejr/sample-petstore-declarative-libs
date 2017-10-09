65190449bcea18e179f8fce1a664b58b860e488e
Steve Richards <srichards@leftshiftit.com>
2017-10-09 20:37:28 +0200

    Added separate git var to push files

---
+++ vars/gitCommitPush.groovy
@@ -0,0 +1,20 @@
+/**
+ * Created by steve-leftshift on 09/10/2017.
+ */
+
+def call(Map config) {
+
+    // push back
+    def branchName = sh(returnStdout: true, script: "git symbolic-ref --short HEAD").trim()
+    def message = "Updated ${config.component} address to value ${config.address}"
+
+    sh "git config user.email \"${config.git-email}\""
+    sh "git config --global user.name \"${config.git-user}\""
+
+    sh "git add ${config.file}"
+    sh "git commit -m \"${message}\""
+
+    withCredentials([usernamePassword(credentialsId: config.git-credentials, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+        sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${config.repo}")
+    }
+}
